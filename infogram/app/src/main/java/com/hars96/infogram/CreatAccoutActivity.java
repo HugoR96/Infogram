be9433758f7afb7b3eaf9970d54ff0484b368b68
HugoR96 <162131124+HugoR96@users.noreply.github.com>
2025-09-19 08:00:02 -0400

    Add files via upload

---
+++ infogram/app/src/main/java/com/hars96/infogram/CreatAccoutActivity.java
@@ -0,0 +1,28 @@
+package com.hars96.infogram;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CreatAccoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_creat_accout);
+            showToolBar(getString(R.string.title_creat_cuenta), true);
+        }
+    public void showToolBar(String titulo, boolean botonSubir) {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+}
